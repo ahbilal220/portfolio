e5f9fc1874249309945a3cd6c310fbfe7da08e20
ahbilal220 <ahbilal220@gmail.com>
2025-08-05 19:45:57 +0500

    Added title

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import About from './components/About';
 import Contact from './components/Contact';
 import Hero from './components/Hero';
@@ -6,6 +7,10 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 
 function App() {
+  useEffect(() => {
+    document.title = "Ahmad Bilal | Portfolio";
+  }, []);
+
   return (
     <>
       <Navbar />
